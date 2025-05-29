b65525ca7962cfdc72291289e3aef8faac0702ba
xmemxa <eszczech3@gmail.com>
2025-05-29 18:13:54 +0200

    feat(auth): add reusable role-based middleware for admin and user

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,13 @@ Route::get('/', function () {
     return view('pages.landingpage');
 });
 
-Route::prefix('admin')->group(function () {
+Route::middleware(['auth', 'check.roles:admin'])->prefix('admin')->group(function () {
     Route::resource('events', EventController::class);
 });
 
+Route::middleware(['auth', 'check.roles:user'])->group(function () {
+
+});
 
 Route::get('/events', [EventController::class, 'index'])->name('events.index');
 Route::get('/events/{event}', [EventController::class, 'show'])->name('events.show');
